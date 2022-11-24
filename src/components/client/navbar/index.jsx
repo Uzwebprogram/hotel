b7f0ebe0458273deef9d2e9f4f63c86a2216fdbf
suprablack <suprablack12@gmail.com>
2022-11-24 11:08:34 +0300

    assets

--- src/components/client/navbar/index.jsx
+++ src/components/client/navbar/index.jsx
@@ -1,11 +1,38 @@
-
-
+import Logo from "../../../assets/client/header/Logo.png"
 
 
 function Navbar() {
-    return(
+    return (
         <>
-        nav
+            <Nav>
+                <img src={Logo} alt="logo" />
+                <ul>
+                    <li>
+                        <a href="#">Галерея</a>
+                    </li>
+                    <li>
+                        <a href="#">Об отеле</a>
+                    </li>
+                    <li>
+                        <a href="#">Номера</a>
+                    </li>
+                    <li>
+                        <a href="#">Отзывы</a>
+                    </li>
+                    <li>
+                        <a href="#">Контакты</a>
+                    </li>
+                    <li>
+                        <a href="#">+998 99 999 99 99</a>
+                        <a href="#">+998 99 999 99 99</a>
+                    </li>
+                    <li>
+                        <select>
+                            <option value="ru">uz</option>
+                        </select>
+                    </li>
+                </ul>
+            </Nav>
         </>
     );
 }
