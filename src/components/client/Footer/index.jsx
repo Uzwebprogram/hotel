6af559da1123c6bd3bf55ab22568eead60e4ecca
munisakhan <munisakhan2005@gmail.com>
2022-11-24 13:16:32 +0500

    footer

--- src/components/client/Footer/index.jsx
+++ src/components/client/Footer/index.jsx
@@ -1,9 +1,85 @@
 
+import {Wrapper, WrapperContainer, Title, Heading,Ul, P} from "./styled-index"
 
 function Footer() {
         return(
             <>
-            Footer
+             <Wrapper>
+                <WrapperContainer>
+                    <Title>
+                        <h2>Logo</h2>
+                        <Heading>
+                            <ul>
+                                <li>
+                                <a href="#">Галерея</a>
+                                </li>
+                                <li>
+                                    <a href="#">Об отеле</a>
+                                </li>
+                                <li>
+                                    <a href="#">Номера</a>
+                                </li>
+                                <li>
+                                    <a href="#">Отзывы</a>
+                                </li>
+                                <li>
+                                    <a href="#">Правила и условия</a>
+                                </li>
+                                <li>
+                                    <a href="#">Контакты</a>
+                                </li>
+                            </ul>
+
+                            <ul>
+                                <li>
+                                <a href="#">Однокомнатный номер</a>
+                                </li>
+                                <li>
+                                    <a href="#">Однокомнатный люкс</a>
+                                </li>
+                                <li>
+                                    <a href="#">Двухкомнатный номер</a>
+                                </li>
+                                <li>
+                                    <a href="#">Двухкомнатный люкс</a>
+                                </li>
+                                <li>
+                                    <a href="#">Семейный номер</a>
+                                </li>
+                                <li>
+                                    <a href="#">Семейный люкс</a>
+                                </li>
+                            </ul>
+
+                            
+                           <ul>
+                            <li>
+                               <a href="#">г.Ташкент Чиланзарский район ул.Бунёдкор д99</a>
+                            </li>
+                            <li>
+                                <a href="#">+99893453455</a>
+                            </li>
+                            <li>
+                                <a href="#">+99898345345</a>
+                            </li>
+                            <li>
+                                <a href="#">revel@mail.ru</a>
+                            </li>
+                            <li>
+                                <i class='bx bxl-instagram-alt' ></i>
+                                <i class='bx bxl-telegram' ></i>
+                                <i class='bx bxl-facebook-circle'></i>
+                            </li>
+                            </ul>
+                        </Heading>
+
+                        <Ul>
+                      
+                        </Ul>
+                    </Title>
+                    <P></P>
+                </WrapperContainer>
+            </Wrapper>
             </>
         )
 }
---
+++ src/components/client/Footer/styled-index.js
@@ -0,0 +1,41 @@
+import styled from "styled-components"
+
+export const Wrapper = styled.div`
+ background-color: #0F0F11;
+;
+`
+
+export const WrapperContainer = styled.div`
+
+a{
+    color: #FFFFFF;
+    text-decoration: none;
+    font-family: 'Gilroy';
+    font-style: normal;
+    font-weight: 400;
+    font-size: 16px;
+    line-height: 120%;
+    letter-spacing: 0.01em;
+}
+ul{
+    list-style: none;
+}
+
+`
+
+export const Title = styled.div`
+ h2{
+    color: #FFFFFF;
+ }
+`
+
+export const Heading = styled.div`
+display: flex;
+`
+export const Ul = styled.div`
+
+`
+
+export const P = styled.div`
+
+`
