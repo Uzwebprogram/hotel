8d7d1bfa29e5ca3758a49f26a464711e2aaff3b7
suprablack <suprablack12@gmail.com>
2022-11-24 11:21:31 +0300

    Header component

--- src/components/client/Header/index.jsx
+++ src/components/client/Header/index.jsx
@@ -1,13 +1,41 @@
 import { WrapperContainer } from "../../../App-styled"
-import Navbar from "../navbar"
-import { Wrapper } from "./styled-index"
+import Logo from "../../../assets/client/header/Logo.png"
+import { Wrapper , Nav} from "./styled-index"
 
 
 function Header() {
     return (
         <Wrapper>
             <WrapperContainer>
-                <Navbar/>
+                <Nav>
+                    <img src={Logo} alt="logo" />
+                    <ul>
+                        <li>
+                            <a href="#">Галерея</a>
+                        </li>
+                        <li>
+                            <a href="#">Об отеле</a>
+                        </li>
+                        <li>
+                            <a href="#">Номера</a>
+                        </li>
+                        <li>
+                            <a href="#">Отзывы</a>
+                        </li>
+                        <li>
+                            <a href="#">Контакты</a>
+                        </li>
+                        <li>
+                            <a href="#">+998 99 999 99 99</a>
+                            <a href="#">+998 99 999 99 99</a>
+                        </li>
+                        <li>
+                            <select>
+                                <option value="ru">uz</option>
+                            </select>
+                        </li>
+                    </ul>
+                </Nav>
             </WrapperContainer>
         </Wrapper>
     )
--- src/components/client/Header/styled-index.js
+++ src/components/client/Header/styled-index.js
@@ -4,3 +4,29 @@ import styled from "styled-components";
 export const Wrapper = styled.div`
 background: #0F0F11;
 `
+
+
+export const Nav = styled.div`
+display: flex;
+align-items: center;
+justify-content: space-between;
+
+ul{
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 65%;
+
+    li{
+        a{
+            text-decoration: none;
+            color: white;
+        }
+    }
+    li:nth-child(6) {
+        display: flex;
+        flex-direction: column;
+    }
+}
+`
